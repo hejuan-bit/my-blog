8fc7dfdb39b2edd81c9c83c3f2e9e47492588c3a
hejuan-bit <1493826046@qq.com>
2023-10-24 21:37:12 +0800

    搭建页面基础框架

---
+++ components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>底部</div>
+    )
+}
+
+export default Footer;
---
+++ components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>导航</div>
+    )
+}
+
+export default Navbar;
