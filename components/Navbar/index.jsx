c3eda83ca2019bd3ce1bb06c3135e3014998d2b3
hejuan-bit <1493826046@qq.com>
2023-10-26 00:24:45 +0800

    调整顶部导航样式

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,22 +1,34 @@
+'use client'
+
 import React from "react";
 import { navs } from "./config";
 import Link from 'next/link';
+import { usePathname } from "next/navigation";
 import style  from "./index.module.scss";
 
 const Navbar = () => {
+    const pathname = usePathname()
     return (
         <div className={style.navbar}>
             <section className={style.logArea}>BLOG-C</section>
             <section>
-                {
-                    navs?.map(nav => {
-                        return(
-                            <Link key={nav.label} href={nav?.value} className={style.linkArea}>
-                                {nav.label}
-                            </Link>
-                        )
-                    })
-                }
+                <ul className={style.linkwrap}>
+                    {
+                        navs?.map(nav => {
+                            return(
+                                <li key={nav.label} className={style.linkArea}>
+                                    <Link 
+                                        href={nav?.value}
+                                        className={pathname?.startsWith(nav?.value) ? style.active : ''}
+                                    >
+                                        {nav.label}
+                                    </Link>
+                                </li>
+                               
+                            )
+                        })
+                    }
+                </ul>
             </section>
         </div>
     )
